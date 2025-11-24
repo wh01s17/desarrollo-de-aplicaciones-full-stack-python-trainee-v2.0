eeafcdb12dd7806f04fcb66b17537ce0820300c4
wh01s17 <jor.perezo@hotmail.com>
2025-11-24 13:28:30 -0300

    refactor ejemplo-calculadora

--- RTD-24-01-14-0039-1/modulo-02/leccion-06/clase-01/ejemplo-calculadora/assets/js/script.js
+++ RTD-24-01-14-0039-1/modulo-02/leccion-06/clase-01/ejemplo-calculadora/assets/js/script.js
@@ -1,29 +1,23 @@
 const resultado = $("#resultado");
+const num1 = Number($("#num1").val());
+const num2 = Number($("#num2").val());
 
 $("#sumar").click(function () {
-    const num1 = Number($("#num1").val());
-    const num2 = Number($("#num2").val());
     const total = num1 + num2;
     resultado.text(total);
 });
 
 $("#restar").click(function () {
-    const num1 = Number($("#num1").val());
-    const num2 = Number($("#num2").val());
     const total = num1 - num2;
     resultado.text(total);
 });
 
 $("#multiplicar").click(function () {
-    const num1 = Number($("#num1").val());
-    const num2 = Number($("#num2").val());
     const total = num1 * num2;
     resultado.text(total);
 });
 
 $("#dividir").click(function () {
-    const num1 = Number($("#num1").val());
-    const num2 = Number($("#num2").val());
     const total = num1 / num2;
     resultado.text(total);
 });
