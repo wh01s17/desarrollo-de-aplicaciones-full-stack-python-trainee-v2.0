d3ab8ca3d69bf2e583e37488b039fb2860c2a679
wh01s17 <jor.perezo@hotmail.com>
2025-11-25 14:49:15 -0300

    reorganized folders

--- RTD-24-01-10-0037-1/modulo-03/leccion-01/clase-01/func_while.py
+++ RTD-24-01-10-0037-1/modulo-03/leccion-01/clase-01/func_while.py
@@ -1,10 +1,9 @@
 # Ejercicio 1
 # Solicitar numeros al usuario y sumarlo, cuando ingrese 0, el programa termina e imprime la suma total
-
 total = 0
 numero = None
 
-while numero !=0 :
+while numero != 0:
     numero = int(input("Ingrese un numero (0 para salir):"))
     total += numero
 
@@ -18,4 +17,4 @@ clave = ""
 while clave != clave_correcta:
     clave = input("Ingrese la contraseña:")
 
-print("Acceso cocedido")
+print("Acceso concedido")
