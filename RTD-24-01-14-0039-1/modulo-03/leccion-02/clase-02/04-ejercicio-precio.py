f89ec7db043dd9be854c206a8cc546938e651be5
wh01s17 <jor.perezo@hotmail.com>
2025-12-02 18:09:23 -0300

    fix exercise 04 lesson 03, 039-1

--- RTD-24-01-14-0039-1/modulo-03/leccion-02/clase-02/04-ejercicio-precio.py
+++ RTD-24-01-14-0039-1/modulo-03/leccion-02/clase-02/04-ejercicio-precio.py
@@ -1,6 +1,6 @@
 """
 Ejercicio 4
-Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades y muestre por pantalla una cadena con el nombre del producto seguido de su precio unitario con 6 dígitos enteros y 2 decimales, el número de unidades con tres dígitos y el coste total con 8 dígitos enteros y 2 decimales.
+Escribir un programa que pregunte el nombre de un producto, su precio y un número de unidades y muestre por pantalla una cadena con el nombre del producto seguido de su precio unitario con 6 dígitos enteros y 2 decimales, el número de unidades con 3 dígitos y el coste total con 8 dígitos enteros y 2 decimales.
 """
 
 nombre_producto = input("Ingrese el nombre del producto: ")
@@ -9,6 +9,7 @@ unidades = int(input("Ingrese cantidad de unidades: "))
 
 total = precio * unidades
 
+print("Nombre producto:", nombre_producto)
 print(f"Precio unitario: {precio:09.2f}")
 print(f"Unidades: {unidades:03d}")
 print(f"Coste total: {total:011.2f}")
