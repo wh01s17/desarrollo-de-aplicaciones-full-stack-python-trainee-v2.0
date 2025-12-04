7d44a07bc1c061518ac81dac9fbfc8bac586f3f7
wh01s17 <jor.perezo@hotmail.com>
2025-12-04 17:55:31 -0300

    fix exercise 1

--- RTD-24-01-14-0039-1/modulo-03/leccion-03/clase-02/01-ejercicio-num-impar.py
+++ RTD-24-01-14-0039-1/modulo-03/leccion-03/clase-02/01-ejercicio-num-impar.py
@@ -5,7 +5,7 @@ input_user = int(input("Ingrese un numero entero positivo: "))
 for i in range(input_user):
     num = i + 1
     if num % 2 == 1:
-        print(num)
+        print(num, end=",")
 
 print("\n")
 
@@ -13,11 +13,11 @@ print("\n")
 # Sin variable auxiliar
 for i in range(1, input_user + 1):
     if i % 2 == 1:
-        print(i)
+        print(i, end=",")
 
 print("\n")
 
 # Opcion 3
 # Empieza en 1 y suma de 2 en 2
 for i in range(1, input_user + 1, 2):
-    print(i)
+    print(i, end=",")
