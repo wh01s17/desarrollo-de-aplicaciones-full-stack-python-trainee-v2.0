0f809ae5791fc8795c5fd1dbe7a24b0b72285d26
wh01s17 <jor.perezo@hotmail.com>
2025-12-04 16:48:48 -0300

    added exercises lesson 3 class 2 0039-1

---
+++ RTD-24-01-14-0039-1/modulo-03/leccion-03/clase-02/00-ejemplo-bucles.py
@@ -0,0 +1,38 @@
+# WHILE
+
+"""
+el bucle while se utiliza cuando no sabemos cuantas veces se va a iterar
+condicion de entrada
+while (condicion):
+    codigo a ejecutar
+    condicion de salida
+"""
+
+# numero = 0
+
+# while numero < 10:
+#     print(numero)
+#     numero += 1
+
+# print("mensaje")
+
+# while True:
+#     opcion = input("Dime tu nombre o presiona enter para salir: ")
+
+#     if not opcion:
+#         print(opcion)
+
+# print("mensaje")
+
+
+# Ciclo FOR
+
+"""
+for varible in iterable
+    codigo a ejecutar
+"""
+
+string = "Python"
+
+for letra in string:
+    print(letra)
---
+++ RTD-24-01-14-0039-1/modulo-03/leccion-03/clase-02/02-ejercicio-triangulo.py
@@ -0,0 +1,4 @@
+altura = int(input("Ingrese un número entero positivo para la altura del triángulo: "))
+
+for i in range(1, altura + 1):
+    print("*" * i)
